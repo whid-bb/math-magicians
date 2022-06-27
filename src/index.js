5bc0e1908750116047f569171eb3e3e60b31b05c
whid-bb <whid11s50@gmail.com>
2022-06-27 11:55:32 +0200

    add react

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const MathMagicians = () => (
+  <div>
+    <h1>
+      Welcome to Math Magicians.
+    </h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<MathMagicians />);
